834f68e32429f9497e74586d4c352273920f631f
edgar wang <edgarwang95@gmail.com>
2018-01-24 18:50:02 -0800

    added another key

--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -17,7 +17,7 @@ const {
 const MapWithASearchBox = compose(
   withProps({
     googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places',
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyB7Xvojsa3NI5BrS3wwNjfpU4mAG5Ewmqk&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `300px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
